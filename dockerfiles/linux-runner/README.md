030d14e32c8aa860a93793c7effe06b17de9c44b
tdemeester <git@demeester.dev>
2022-08-19 12:09:11 +0000

    add github runner container definition

---
+++ dockerfiles/linux-runner/README.md
@@ -0,0 +1,7 @@
+# containerized runner for github
+
+runner created to run in azure container app service.  
+it has installed as extra:
+  - azcli
+  - node
+
---
+++ dockerfiles/linux-runner/scripts/githubrunner-debian.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Thijs De Meester. All rights reserved.
+# Licensed under the MIT License. 
+#-------------------------------------------------------------------------------------------------------------
+#
+# Syntax: ./githubrunner-debian.sh [directory to install runner] [github runner version to install (use "latest" to install latest)] [non-root user]
+
+export INSTALL_DIR=${1:-"/usr/local/share/actions-runner"}
+export RUNNER_VERSION=${2:-"latest"}
+USERNAME=${3:-"root"}
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+if [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+# adjust version
+if [ "${RUNNER_VERSION}" = "latest" ]; then
+    export RUNNER_VERSION=$(curl -so- https://api.github.com/repos/actions/runner/releases/latest | jq --raw-output .name)
+fi
+if [ "${RUNNER_VERSION:0:1}" = "v" ]; then
+    export RUNNER_VERSION="${RUNNER_VERSION:1}"
+fi
+
+if [ ! -d "${INSTALL_DIR}" ]; then
+    mkdir -p "${INSTALL_DIR}"
+fi
+
+# download runner
+cd "${INSTALL_DIR}"
+curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
+tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
+
+echo "installing dependacies"
+./bin/installdependencies.sh
+chown -R "${USERNAME}" "${INSTALL_DIR}"
