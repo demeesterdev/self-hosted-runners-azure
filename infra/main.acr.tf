b21f3e1d7fd9d79038f8ec5e6590fe691f017368
tdemeester <git@demeester.dev>
2022-09-09 14:04:15 +0000

    fix resource naming and scaling

--- infra/main.acr.tf
+++ infra/main.acr.tf
@@ -48,13 +48,13 @@ resource "azurerm_container_registry_task_schedule_run_now" "runner_build_task_l
 }
 
 resource "azurerm_private_endpoint" "runner_acr" {
-  name                = "${var.container_app_name}-acr-endpoint"
+  name                = "${var.registry_name}-endpoint"
   resource_group_name = azurerm_resource_group.runner_group.name
   location            = azurerm_resource_group.runner_group.location
   subnet_id           = azurerm_subnet.acr.id
 
   private_service_connection {
-    name                           = "${var.container_app_name}-acr-privateserviceconnection"
+    name                           = "${var.registry_name}-privateserviceconnection"
     private_connection_resource_id = azurerm_container_registry.runner_acr.id
     is_manual_connection           = false
     subresource_names              = ["registry"]
--- infra/main.auto.tfvars
+++ infra/main.auto.tfvars
@@ -1,6 +1,7 @@
 resource_group_name                   = "demapp-ghrunner-demo"
 location                              = "west europe"
 registry_name                         = "demappghrunnerdemoacr"
+registry_agent_pool_tier              = "S2"
 registry_build_task_name              = "ghrunner-build"
 container_build_image_name            = "ghrunner"
 container_build_linux_context         = "https://github.com/demeesterdev/self-hosted-runners-azure#main:dockerfiles/linux-runner"
